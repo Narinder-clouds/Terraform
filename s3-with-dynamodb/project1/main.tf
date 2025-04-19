ecdc3550308e07d4cc7867db9a908f3336b9bfb2
Prince <princesingh0123ps@gmail.com>
2025-04-19 14:34:57 +0530

    Enable locking file Mechanism

---
+++ s3-with-dynamodb/project1/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "server" {
+     instance_type = "t2.micro"
+     ami = "ami-0e2c8caa4b6378d8c"
+     tags = {
+       "name" = "ec2 of project-1"
+     }
+}
+terraform {
+  backend "s3" {
+    bucket = "aizenn"
+    key = "terraform/terrafrom.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "locking-file-table"
+  }
+}
